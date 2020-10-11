d94e163289d9e6afc25fb892ea418ecb7b94f618
liangyehao <1094311509@qq.com>
2020-10-12 00:35:54 +0800

    整合threejs,打包成桌面应用

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Demo01 from "./3d/Demo01";
+import Demo02 from "./3d/Demo02";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Demo01 />
+    <Demo02 />
   </React.StrictMode>,
   document.getElementById('root')
 );
