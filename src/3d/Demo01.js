f87718363a90414bcfa85faf293dbd68fd49334b
liangyehao <1094311509@qq.com>
2020-10-11 22:02:17 +0800

    3d分支

---
+++ src/3d/Demo01.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Demo01 extends Component{
+
+    render() {
+        return <div>111111</div>;
+    }
+}
+
+export default Demo01;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Demo01 from "./3d/Demo01";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Demo01 />
   </React.StrictMode>,
   document.getElementById('root')
 );
